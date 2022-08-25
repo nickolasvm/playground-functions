5a7ed198295f187c4bba47598a19703d120703d5
nickolasvm <nickolas64vm@gmail.com>
2022-08-25 15:18:06 -0300

    resolução desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,4 @@
 // Desafio 11
-// let teste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
 function generatePhoneNumber(telNum) {
   // seu código aqui
   if (telNum.length !== 11) {
@@ -7,24 +6,38 @@ function generatePhoneNumber(telNum) {
   }
 
   let resultado = '';
+  let contador = 0;
+
   for (let i = 0; i < telNum.length; i += 1) {
     if (telNum[i] > 9 || telNum[i] < 0) {
-      // TODO: colocar statemnet sobre aparacer 3 veze so msm numero
       return 'não é possível gerar um número de telefone com esses valores';
     }
+
+    // faz um loop nos elementos 1 casa depois do atual, para checagem de duplicados
+    for (j = i + 1; j < telNum.length; j += 1) {
+      if (telNum[i] === telNum[j]) {
+        contador += 1;
+      }
+    }
+
+    // contador >= 2 pois o números que está sendo avaliado não entra na soma
+    if (contador >= 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+
+    // reseta o contador de duplicados para o próximo número
+    contador = 0;
+
     resultado = resultado.concat(telNum[i]);
   }
 
-  resultado =
-    '(' +
-    resultado.slice(0, 2) +
-    ') ' +
-    resultado.slice(2, 7) +
-    '-' +
-    resultado.slice(7, 11);
+  resultado = '(' + resultado.slice(0, 2) 
+    + ') ' + resultado.slice(2, 7) 
+    + '-' + resultado.slice(7, 11)
+  ;
+
   return resultado;
 }
-// console.log(generatePhoneNumber(teste))
 
 // Desafio 12
 function triangleCheck() {
